0d595c1994cc8b2636e39154622d165d3827ae83
Tjark <tjarksievers@icloud.com>
2024-05-06 13:47:15 +0200

    Retry some failed jobs

---
+++ code/submit_jobs_20240506.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+code/submit_job.sh 0.0 9.5
+code/submit_job.sh 0.0 10.1
+code/submit_job.sh 0.0 10.3
+code/submit_job.sh 0.0 10.9
+code/submit_job.sh 0.0 11.1
+code/submit_job.sh 0.0 11.3
+code/submit_job.sh 0.0 11.9
+code/submit_job.sh 0.0 12.5
+code/submit_job.sh 0.0 12.7
+code/submit_job.sh 0.0 12.9
+code/submit_job.sh 0.0 13.1
+code/submit_job.sh 0.0 13.3
+code/submit_job.sh 0.0 13.7
+code/submit_job.sh 0.0 13.9
