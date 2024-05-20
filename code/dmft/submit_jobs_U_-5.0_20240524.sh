57ef223e2ad4ffeb78ca288c146a928e010fa8ad
Tjark <tjarksievers@icloud.com>
2024-05-20 14:30:15 +0200

    Run jobs with varying U

---
+++ code/dmft/submit_jobs_U_-5.0_20240524.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+code/dmft/submit_job.sh -5.0 1.0
+code/dmft/submit_job.sh -5.0 2.0
+code/dmft/submit_job.sh -5.0 3.0
+code/dmft/submit_job.sh -5.0 4.0
+code/dmft/submit_job.sh -5.0 5.0
+code/dmft/submit_job.sh -5.0 6.0
+code/dmft/submit_job.sh -5.0 7.0
+code/dmft/submit_job.sh -5.0 8.0
+code/dmft/submit_job.sh -5.0 9.0
+code/dmft/submit_job.sh -5.0 10.0
+code/dmft/submit_job.sh -5.0 11.0
+code/dmft/submit_job.sh -5.0 12.0
+code/dmft/submit_job.sh -5.0 13.0
+code/dmft/submit_job.sh -5.0 14.0
+code/dmft/submit_job.sh -5.0 15.0
+code/dmft/submit_job.sh -5.0 16.0
+code/dmft/submit_job.sh -5.0 17.0
+code/dmft/submit_job.sh -5.0 18.0
+code/dmft/submit_job.sh -5.0 19.0
+code/dmft/submit_job.sh -5.0 20.0
+code/dmft/submit_job.sh -5.0 21.0
+code/dmft/submit_job.sh -5.0 22.0
+code/dmft/submit_job.sh -5.0 23.0
+code/dmft/submit_job.sh -5.0 24.0
+code/dmft/submit_job.sh -5.0 25.0
---
+++ code/dmft/submit_jobs_U_-6.0_20240524.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+code/dmft/submit_job.sh -6.0 1.0
+code/dmft/submit_job.sh -6.0 2.0
+code/dmft/submit_job.sh -6.0 3.0
+code/dmft/submit_job.sh -6.0 4.0
+code/dmft/submit_job.sh -6.0 5.0
+code/dmft/submit_job.sh -6.0 6.0
+code/dmft/submit_job.sh -6.0 7.0
+code/dmft/submit_job.sh -6.0 8.0
+code/dmft/submit_job.sh -6.0 9.0
+code/dmft/submit_job.sh -6.0 10.0
+code/dmft/submit_job.sh -6.0 11.0
+code/dmft/submit_job.sh -6.0 12.0
+code/dmft/submit_job.sh -6.0 13.0
+code/dmft/submit_job.sh -6.0 14.0
+code/dmft/submit_job.sh -6.0 15.0
+code/dmft/submit_job.sh -6.0 16.0
+code/dmft/submit_job.sh -6.0 17.0
+code/dmft/submit_job.sh -6.0 18.0
+code/dmft/submit_job.sh -6.0 19.0
+code/dmft/submit_job.sh -6.0 20.0
+code/dmft/submit_job.sh -6.0 21.0
+code/dmft/submit_job.sh -6.0 22.0
+code/dmft/submit_job.sh -6.0 23.0
+code/dmft/submit_job.sh -6.0 24.0
+code/dmft/submit_job.sh -6.0 25.0
