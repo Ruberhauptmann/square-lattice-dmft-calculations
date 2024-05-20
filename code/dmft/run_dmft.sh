d150e9b2db57be30fbe7a28d4f2ddd7121fff348
Tjark <tjarksievers@icloud.com>
2024-05-20 13:20:02 +0200

    Fix error in jobscript

--- code/dmft/run_dmft.sh
+++ code/dmft/run_dmft.sh
@@ -6,13 +6,13 @@ beta=$2
 CODE_DIR=${PWD}/code
 
 # Prepare result directory with input files
-mkdir -p "dmft_results/mu_${U}/beta_${beta}"
+mkdir -p "dmft_results/U_${U}/beta_${beta}"
 
 python3 code/dmft/generate_inputs.py -U ${U} --beta ${beta}
 cp "input/mu_${mu}/beta_${beta}/dmft.input" "dmft_results/U_${U}/beta_${beta}/"
 cp "input/HR.txt" "dmft_results/U_${U}/beta_${beta}/"
 
-cd "dmft_results/mu_${U}/beta_${beta}"
+cd "dmft_results/U_${U}/beta_${beta}"
 
 # Run the actual DMFT loop in the result directory (so all files get placed there)
 "${CODE_DIR}"/dmft/dmft_loop.sh
