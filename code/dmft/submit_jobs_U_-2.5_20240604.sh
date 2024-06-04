3ed08a0c3bc2970615ed8ef95fb9f40a3e202b66
Tjark <tjarksievers@icloud.com>
2024-06-04 10:36:47 +0200

    New jobs

---
+++ code/dmft/submit_jobs_U_-2.5_20240604.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+code/dmft/submit_job.sh -2.5 8.5
+code/dmft/submit_job.sh -2.5 9.25
+code/dmft/submit_job.sh -2.5 9.75
+code/dmft/submit_job.sh -2.5 10.25
+code/dmft/submit_job.sh -2.5 10.75
+code/dmft/submit_job.sh -2.5 11.25
+code/dmft/submit_job.sh -2.5 11.75
+code/dmft/submit_job.sh -2.5 12.25
---
+++ code/dmft/submit_jobs_U_-3.5_20240604.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+code/dmft/submit_job.sh -3.5 5.5
+code/dmft/submit_job.sh -3.5 6.6
+code/dmft/submit_job.sh -3.5 7.5
+code/dmft/submit_job.sh -3.5 8.5
