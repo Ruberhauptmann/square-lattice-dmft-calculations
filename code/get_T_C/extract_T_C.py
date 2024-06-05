6ee44ab8a3bc15ad7f90d237e871a188b4d1f166
Tjark <tjarksievers@icloud.com>
2024-06-05 10:58:25 +0200

    Fix script for extracting T_C

--- code/get_T_C/extract_T_C.py
+++ code/get_T_C/extract_T_C.py
@@ -21,7 +21,6 @@ if __name__ == "__main__":
     plot_path = Path(sys.argv[-1])
     plot_path.mkdir(exist_ok=True, parents=True)
     csv_path = Path(sys.argv[-2])
-    csv_path.mkdir(exist_ok=True, parents=True)
 
     critical_temperatures = pd.DataFrame(
         columns=["U", "T_C", "beta_C"], index=range(len(U_list))
